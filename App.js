de668376e77622f94e6fc0169932512069c9db9b
fierazahra <162279201+fierazahra@users.noreply.github.com>
2024-05-22 22:50:18 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hi, this is me - Fiera!</h1>
+      <p>Right now, I am 18 years old and I live in Bandung.</p>
+        <p>
+        My full name is Fiera Zahra Salsabila and I am a digital business student.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
